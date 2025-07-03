030372b9f8f6a8c08b56eb7c7310897b7cd1998e
Admin <Admin@DESKTOP-PF0VNFN>
2025-07-03 11:24:09 +0530

    commit

---
+++ TNSIF_JavaProgram/src/com/tnsif/nonaccessmodifiers/FinalDemo.java
@@ -0,0 +1,21 @@
+package com.tnsif.nonaccessmodifiers;
+
+final class FinalDemo {
+	
+	final int Max = 100;
+	
+	 void display() {
+		System.out.println("show the value of max: "+Max);
+	}
+
+	public static void main(String[] args) {
+		FinalDemo fd = new FinalDemo();
+		fd.display();
+		
+
+	}
+
+}
+
+
+
